ed65a94cce6022ac1af22efad6a68cb028bd843f
KarimParis1 <Karim.Boutouil@malix.univ-paris1.fr>
2016-03-14 14:01:22 +0100

    Karim
    Cross Origin OK

--- Web/Vue Etudiante/app/controlers/listeMatiereCtrl.js
+++ Web/Vue Etudiante/app/controlers/listeMatiereCtrl.js
@@ -4,13 +4,21 @@ var listeMatiereCtrl = angular.module('listeMatiereCtrl', []);
 listeMatiereCtrl.controller('listeMatiereCtrl', function ($scope,$http) {
     console.log("listeQuizzCtrl");
 
+    /*
     $http.get('app/requetes/matiere.json').then(function(response){
         $scope.matieres=response.data;
         console.log($scope.matieres);
     },function(reason){
         console.log(reason);
     });
+*/
 
+    $http.get('http://localhost:8080/listeMatieres').then(function(response){
+        $scope.matieres=response.data;
+        console.log(response.data);
+    },function(reason){
+        console.log(reason);
+    });
 
 
 
