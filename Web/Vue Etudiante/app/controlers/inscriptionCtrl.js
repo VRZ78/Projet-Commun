4c4ce9f96a45173c7fa1359a442aa657c63500ef
KarimParis1 <Karim.Boutouil@malix.univ-paris1.fr>
2016-03-18 16:28:39 +0100

    inscription

--- Web/Vue Etudiante/app/controlers/inscriptionCtrl.js
+++ Web/Vue Etudiante/app/controlers/inscriptionCtrl.js
@@ -41,21 +41,20 @@ inscriptionCtrl.controller('inscriptionCtrl', function ($scope, $http,$filter, L
         year: "",
         typeCompte: 1
     };
-
     // Vérification du formulaire d'inscritpion
     $scope.isCreateButtonDisabled = function () {
         return false;
     };
-
-
     // Fonction appelé lors du click sur le bouton de création de compte
     $scope.accountConfirmCreation = function (quiz) {
+        $scope.inscEleve.birthday = $filter('date')($scope.inscEleve.birthday, "yyyy-MM-dd");
         $http.post('http://localhost:8080/inscription', JSON.stringify($scope.inscEleve)).then(function (response) {
-            $scope.inscEleve.birthday = $filter('date')($scope.inscEleve.birthday, "yyyy-MM-dd");
             LxNotificationService.success('Votre compte a bien été créé. Merci de vérifier vos mails et de cliquer sur le lien d activation');
             console.log(response);
             $scope.hasAccountCreationButtonBeenClicked = true;
         }, function () {
+            console.log("http://localhost:8080/inscription");
+            console.log($scope.inscEleve);
             LxNotificationService.error('Impossible de contacter le serveur');
         });
 
